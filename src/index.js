9dbfcf9e550b119827f53410e9e415218f4da709
sergeyvkruk <dlyareferata2014@gmail.com>
2021-03-16 01:50:44 +0300

    add:result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+            stack.push(str[i]);
+        }
+        else {
+            let last = stack.pop();
+            if (str[i] !== map[last]) {
+                return false
+            };
+        }
+    }
+    if (stack.length !== 0) {
+        return false
+    };
+    return true;}
